881f0603e488e4d75c71ef297ed6cac3db77b25b
Raphaela Mettig <rmettig@Raphaelas-MacBook-Pro.local>
2017-04-22 03:12:10 -0500

    register is responsive

--- app/src/main/java/csc3380/lsu/rmettig/ssid/RegisterActivity.java
+++ app/src/main/java/csc3380/lsu/rmettig/ssid/RegisterActivity.java
@@ -1,13 +1,20 @@
 package csc3380.lsu.rmettig.ssid;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    Shark newShark = null;
+    String sharkType = "";
+    String username = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +24,54 @@ public class RegisterActivity extends AppCompatActivity {
         //gets content from the elements in the form
         final EditText edtNewUsername = (EditText) findViewById(R.id.edtNewUser);
         final Button btnRegister = (Button) findViewById(R.id.btnRegister);
+        final RadioButton rbtGreatWhite = (RadioButton) findViewById(R.id.rbtGreatWhite);
+        final RadioButton rbtHammerHead = (RadioButton) findViewById(R.id.rbtHammerHead);
+        final RadioButton rbtWhale = (RadioButton) findViewById(R.id.rbtWhale);
+
 
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent registerIntent = new Intent(RegisterActivity.this, UserAreaActivity.class);
-                RegisterActivity.this.startActivity(registerIntent);
+
+                if(rbtGreatWhite.isChecked())
+                    sharkType = "greatwhite";
+                if(rbtHammerHead.isChecked())
+                    sharkType = "hammerhead";
+                if(rbtWhale.isChecked())
+                    sharkType = "whale";
+
+                if (!sharkType.equals("")&&!username.equals("")) {
+                    newShark = new Shark(sharkType);
+                    //RegisterActivity.this.startActivity(registerIntent);
+                }
+                else {
+                    new AlertDialog.Builder(RegisterActivity.this).setMessage("You must select an egg!!");
+                }
+            }
+        });
+
+        rbtGreatWhite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rbtHammerHead.setChecked(false);
+                rbtWhale.setChecked(false);
+            }
+        });
+
+        rbtHammerHead.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rbtGreatWhite.setChecked(false);
+                rbtWhale.setChecked(false);
+            }
+        });
+
+        rbtWhale.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rbtGreatWhite.setChecked(false);
+                rbtHammerHead.setChecked(false);
             }
         });
     }
--- app/src/main/java/csc3380/lsu/rmettig/ssid/Welcome.java
+++ app/src/main/java/csc3380/lsu/rmettig/ssid/Welcome.java
@@ -5,32 +5,33 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 
-public class Login extends AppCompatActivity {
+public class Welcome extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_welcome);
 
         final Button btnLogin = (Button) findViewById(R.id.btnLogin);
         final Button btnRegister = (Button) findViewById(R.id.btnRegisterNew);
 
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginIntent = new Intent(Welcome.this, UserAreaActivity.class);
+                startActivity(loginIntent);
+            }
+        });
+
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent registerIntent = new Intent(Login.this, RegisterActivity.class);
+                Intent registerIntent = new Intent(Welcome.this, RegisterActivity.class);
                 startActivity(registerIntent);
             }
         });
 
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent loginIntent = new Intent(Login.this, UserAreaActivity.class);
-                startActivity(loginIntent);
-            }
-        });
+
     }
 }
